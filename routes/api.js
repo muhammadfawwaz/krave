a52d65fb976df62a02ae53de18b80e24753a1bb7
Fif <muhammad.afif81@gmail.com>
2019-11-04 10:32:27 +0700

    oke

--- routes/api.js
+++ routes/api.js
@@ -404,7 +404,7 @@ router.get('/getfnb', async function(req, res, next) {
             url:     'https://developers.zomato.com/api/v2.1/search?entity_type=city&count=8&entity_id=' + req.query.loc,
         }, async function(error, response, body){
             // console.log(body)
-            let resto = body.restaurants
+            let resto = JSON.parse(body.replace(/^\ufeff/g,"")).restaurants
             let result = []
             for(var i in resto) {
                 // console.log(parseInt(resto[i].restaurant.average_cost_for_two))
